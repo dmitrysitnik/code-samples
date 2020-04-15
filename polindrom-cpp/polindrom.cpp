7800a7aa3169744bb8bd4c2f5be5cdc6ad6964ad
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-04-15 22:08:11 +0300

    add polindrom string example to output

--- polindrom-cpp/polindrom.cpp
+++ polindrom-cpp/polindrom.cpp
@@ -11,50 +11,74 @@ Polindrom::~Polindrom()
 {
 }
 
-
-void Polindrom::testShow(char message[]){
+void Polindrom::testShow(char message[])
+{
     std::cout << message << std::endl;
 }
 
-
-void Polindrom::SetInput(std::string input){
+void Polindrom::SetInput(std::string input)
+{
     mInput = input;
 }
 
-
-void Polindrom::InputLength(){
+void Polindrom::InputLength()
+{
     std::cout << "length: " << mInput.length() << std::endl;
 }
 
-
-
-void Polindrom::Process(){
-    std::map <char, int>  mapping;
+void Polindrom::Process()
+{
+    std::map<char, int> mapping;
     int maxPolindrom = 0;
 
     InputLength();
-    
+
+    //Подсчитать кол-во символов в строке
     for (int i = 0; i < mInput.length(); i++)
     {
         mapping[mInput[i]]++;
     }
 
-    for (std::map<char, int>::iterator element = mapping.begin(); element != mapping.end(); element++ )
+    std::string polindromString;
+    std::string polindromTemp;
+    char anyCharWithNoPair;
+
+    for (std::map<char, int>::iterator element = mapping.begin(); element != mapping.end(); element++)
     {
         int possibleInPolidrom = element->second / 2;
+
+        //составляем начальную часть полиндрома или запоминаем нечетный символ
+        if (possibleInPolidrom > 0)
+        {
+            polindromTemp.push_back(element->first);
+        }
+        else
+        {
+            anyCharWithNoPair = element->first;
+        }
+
         maxPolindrom += possibleInPolidrom;
 
         std::cout << "1 = " << element->first << std::endl;
         std::cout << "2 = " << element->second << std::endl;
-
     }
-
+    //По сути расчитано кол-во символов в половине -> умножим на 2
     maxPolindrom *= 2;
 
-    if(mInput.length() % 2 != 0){
+    polindromString = polindromTemp;
+
+    if (mInput.length() % 2 != 0)
+    {
         maxPolindrom += 1;
+        polindromString.push_back(anyCharWithNoPair);
+    }
+
+
+    for (int i = polindromTemp.length() - 1; i >= 0; i--)
+    {
+        polindromString.push_back(polindromTemp[i]);
     }
     
     std::cout << "maxPolindrom: " << maxPolindrom << std::endl;
-    
+    std::cout << "Polindrom string " << polindromString << std::endl;
 }
