eabc2670421181f14227a4dc1cdfb3b7d502ebf9
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-04-22 08:32:51 +0300

    move code to separate file

--- quick-sort-cpp/main.cpp
+++ quick-sort-cpp/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include "quicksort.hpp"
 
-void swap(int *first, int *second)
+void swap2(int *first, int *second)
 {
     int temp = *first;
     *first = *second;
     *second = temp;
 }
 
-int partition(int arr[], int low, int high)
+int partition2(int arr[], int low, int high)
 {
     int pivot = arr[high];
 
@@ -18,22 +19,22 @@ int partition(int arr[], int low, int high)
         if (arr[j] < pivot)
         {
             i++;
-            swap(&arr[i], &arr[j]);
+            swap2(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    swap2(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
 
-void quicksort(int arr[], int low, int high)
+void quicksort2(int arr[], int low, int high)
 {
     if (low < high)
     {
-        int pi = partition(arr, low, high);
+        int pi = partition2(arr, low, high);
 
         //separatly sort left and right parts
-        quicksort(arr, low, pi - 1);
-        quicksort(arr, pi + 1, high);
+        quicksort2(arr, low, pi - 1);
+        quicksort2(arr, pi + 1, high);
     }
 }
 
@@ -49,8 +50,15 @@ int main()
 {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    quicksort(arr, 0, n - 1);
+    quicksort2(arr, 0, n - 1);
     std::cout << "Sorted array: \n";
     printArray(arr, n);
+
+    int arr2[] = {24, 54, 1, 5, 3, 9, 23, 11, 74};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    QuickSort::quicksort(arr2, 0, n2 - 1);
+    std::cout << "Sorted array: \n";
+    printArray(arr2, n2);
+
     return 0;
 }
---
+++ quick-sort-cpp/quicksort.hpp
@@ -0,0 +1,62 @@
+class QuickSort
+{
+private:
+    /* data */
+public:
+    QuickSort(/* args */);
+    ~QuickSort();
+    static void quicksort(int arr[], int low, int high);
+
+    private:
+     static void swap(int* first, int* second);
+     static int partition(int arr[], int low, int high);
+};
+
+QuickSort::QuickSort(/* args */)
+{
+}
+
+QuickSort::~QuickSort()
+{
+}
+
+
+void swap(int *first, int *second)
+{
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+
+    int i = (low - 1); //index of smaller element
+
+    for (int j = low; j <= high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        //separatly sort left and right parts
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+
+
